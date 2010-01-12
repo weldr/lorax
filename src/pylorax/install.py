7eecf20703c4362b019a4e191343a2fea2f8a4f0
Martin Gracik <mgracik@redhat.com>
2010-01-12 12:45:54 +0100

    Added the license

--- src/pylorax/install.py
+++ src/pylorax/install.py
@@ -1,5 +1,24 @@
 #
 # install.py
+# class for creating install images
+#
+# Copyright (C) 2009  Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Red Hat Author(s):  David Cantrell <dcantrell@redhat.com>
+#                     Martin Gracik <mgracik@redhat.com>
 #
 
 import os
@@ -59,6 +78,7 @@ class InstallImage(object):
             dst = os.path.join(self.conf.installtree, "usr", "bin", file)
             shutil.copy2(src, dst)
 
+    # XXX i cannot find this in the repos for f12
     def configure_fedorakmod(self):
         if os.path.isfile(self.paths.FEDORAKMODCONF):
             utils.replace(self.paths.FEDORAKMODCONF,
