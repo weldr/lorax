772cab692c6eeb343753e388652756c56b852529
Martin Gracik <mgracik@redhat.com>
2011-03-04 13:27:59 +0100

    Minor sparc pseudo code changes

--- pseudo/sparc.pseudo.py
+++ pseudo/sparc.pseudo.py
@@ -25,38 +25,38 @@ IMPLANTISOMD5 = "implantisomd5"
 makedirs(joinpaths(outputroot, IMAGESDIR))
 makedirs(joinpaths(outputroot, SPARCDIR))
 
-# create images
-# copy kernel
-kernel.fname = "vmlinuz"
-kernel.fpath = copy(kernel.fpath,
-                    joinpaths(outputroot, SPARCDIR,  kernel.fname))
-
-# create and copy initrd
-initrd_fname = "initrd.img"
-initrd_fpath = copy(create_initrd(kernel),
-                joinpaths(outputroot, SPARCDIR,  initrd_fname))
-
 # copy silo.conf
 siloconf = copy(joinpaths(installroot, ANABOOTDIR, "silo.conf"),
-                joinpaths(outputroot, SPARCDIR, "silo.conf"))
+                joinpaths(outputroot, SPARCDIR))
 
 # copy boot.msg
 bootmsg = copy(joinpaths(installroot, ANABOOTDIR, "boot.msg"),
-                joinpaths(outputroot, SPARCDIR, "boot.msg"))
+               joinpaths(outputroot, SPARCDIR))
 
 replace(bootmsg, "%PRODUCT%", product)
 replace(bootmsg, "%VERSION%", version)
 
-# add kernel and initrd to .treeinfo
-with open(treeinfo, "a") as f:
-    f.write("[images-%s]\n" % kernel.arch)
-    f.write("kernel = %s\n" % joinpaths(SPARCDIR, kernel.fname))
-    f.write("initrd = %s\n" % joinpaths(SPARCDIR, initrd_fname))
-
 # copy  *.b to sparc dir
 copy(joinpaths(installroot, ANABOOTDIR, "*.b"),
-         joinpaths(outputroot, SPARCDIR))
+     joinpaths(outputroot, SPARCDIR))
 
+# create images
+for kernel in kernellist:
+    # copy kernel
+    kernel.fname = "vmlinuz"
+    kernel.fpath = copy(kernel.fpath,
+                        joinpaths(outputroot, SPARCDIR,  kernel.fname))
+
+    # create and copy initrd
+    initrd_fname = "initrd.img"
+    initrd_fpath = copy(create_initrd(kernel),
+                        joinpaths(outputroot, SPARCDIR,  initrd_fname))
+
+    # add kernel and initrd to .treeinfo
+    with open(treeinfo, "a") as f:
+        f.write("[images-%s]\n" % kernel.arch)
+        f.write("kernel = %s\n" % joinpaths(SPARCDIR, kernel.fname))
+        f.write("initrd = %s\n" % joinpaths(SPARCDIR, initrd_fname))
 
 if (bootiso):
     # create isopath dir
@@ -68,19 +68,17 @@ if (bootiso):
 
     # create boot.iso
     bootiso_fpath = joinpaths(outputroot, IMAGESDIR, bootiso)
-    boot_path = joinpaths(outputroot, SPARCDIR)
-    
-    # run mkisofs
+
+    # run mkisofs (XXX what's with the "Fedora" exclude?)
     rc = exec([MKISOFS, "-R", "-J", "-T",
-               "-G", "/boot/isofs.b",
+               "-G", "/%s" % joinpaths(SPARCDIR, "isofs.b"),
                "-B",  "...",
-               "-s",  "/boot/silo.conf", 
-               "-r", "-V", "PBOOT", "-A", '"%s %s"' % (product, version),
-               "-x", "Fedora",
-               "-x", "repodata",
-               "-sparc-label", '"%s %s Boot Disc"' % (product, version), 
+               "-s",  "/%s" % joinpaths(SPARCDIR, "silo.conf"),
+               "-r", "-V", '"PBOOT"', "-A", '"%s %s"' % (product, version),
+               "-x", "Fedora", "-x", "repodata",
+               "-sparc-label", '"%s %s Boot Disc"' % (product, version),
                "-o", bootiso_fpath, "-graft-points",
-               "boot=%s" % boot_path])
+               "boot=%s" % joinpaths(outputroot, SPARCDIR)])
 
     # run implantisomd5
     rc = exec([IMPLANTISOMD5, bootiso_fpath])
