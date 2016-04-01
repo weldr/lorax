c34a0fb535713f9a562414004a82e83d53ddfb37
Brian C. Lane <bcl@redhat.com>
2016-04-01 10:30:04 -0700

    tests: Add discinfo test
    (cherry picked from commit 2e36e941ce7c32b44727ccc6db960f45f1685fa0)

---
+++ tests/pylorax/test_discinfo.py
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2018 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+import unittest
+import tempfile
+
+from pylorax.discinfo import DiscInfo
+
+class DiscInfoTest(unittest.TestCase):
+    def discinfo_test(self):
+        with tempfile.NamedTemporaryFile(mode="w+t") as f:
+            di = DiscInfo("1.0", "x86_64")
+            di.write(f.name)
+            f.seek(0)
+            self.assertTrue(f.readline().strip() not in ["", None])
+            self.assertEqual(f.readline().strip(), "1.0")
+            self.assertEqual(f.readline().strip(), "x86_64")
