2008a413b6b455b7e3adfe355c1a8df940fd5dda
Brian C. Lane <bcl@redhat.com>
2017-10-04 17:03:08 -0700

    lorax-composer initial commit
    The lorax-composer program will launch a BDCS compatible API server
    using Flask and Gevent. Currently this is a skeleton application with
    only one active route (/api/v0/status).
    
    The API code lives in ./src/pylorax/api/v0.py with related code in other
    pylorax/api/* modules.

---
+++ src/pylorax/api/server.py
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2017  Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from flask import Flask, jsonify
+
+from pylorax.api import crossdomain
+from pylorax.api.v0 import v0_api
+
+server = Flask(__name__)
+
+__all__ = ["server"]
+
+@server.route('/')
+@crossdomain(origin="*")
+def hello_world():
+    return 'Hello, World!'
+
+v0_api(server)
---
+++ src/pylorax/api/v0.py
@@ -0,0 +1,44 @@
+#
+# Copyright (C) 2017  Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+from flask import jsonify
+
+# Use pykickstart to calculate disk image size
+from pykickstart.parser import KickstartParser
+from pykickstart.version import makeVersion, RHEL7
+
+from pylorax.api import crossdomain
+from pylorax.creator import DRACUT_DEFAULT, mount_boot_part_over_root
+from pylorax.creator import make_appliance, make_image, make_livecd, make_live_images
+from pylorax.creator import make_runtime, make_squashfs
+from pylorax.imgutils import copytree
+from pylorax.imgutils import Mount, PartitionMount, umount
+from pylorax.installer import InstallError
+from pylorax.sysutils import joinpaths
+
+
+# no-virt mode doesn't need libvirt, so make it optional
+try:
+    import libvirt
+except ImportError:
+    libvirt = None
+
+def v0_api(api):
+    """ Setup v0 of the API server"""
+    @api.route("/api/v0/status")
+    @crossdomain(origin="*")
+    def v0_status():
+        return jsonify(build="devel", api="0", db_version="0", schema_version="0", db_supported=False)
