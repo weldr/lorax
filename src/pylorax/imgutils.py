bf9ed50d510d44137c1da6e5c93cdddcc917a965
Alexander Todorov <atodorov@redhat.com>
2017-10-12 17:24:18 +0300

    Fix dangerous-default value warnings
    when default value is list or dict the default arguments are
    instantiated as objects at the time of definition. This is significant
    (exposing visible semantics) when the object is mutable. There’s no way
    of re-binding that default argument name in the function’s closure. When
    function is executed multiple times with its default value the value
    will change between executions, possibly leading to strange side effects.
    
    For more information see:
    http://satran.in/2012/01/12/python-dangerous-default-value-as-argument.html

--- src/pylorax/imgutils.py
+++ src/pylorax/imgutils.py
@@ -35,11 +35,13 @@ from pylorax.executils import runcmd, runcmd_output
 
 ######## Functions for making container images (cpio, tar, squashfs) ##########
 
-def compress(command, rootdir, outfile, compression="xz", compressargs=["-9"]):
+def compress(command, rootdir, outfile, compression="xz", compressargs=None):
     '''Make a compressed archive of the given rootdir.
     command is a list of the archiver commands to run
     compression should be "xz", "gzip", "lzma", "bzip2", or None.
     compressargs will be used on the compression commandline.'''
+    if compressargs is None:
+        compressargs = ["-9"]
     if compression not in (None, "xz", "gzip", "lzma", "bzip2"):
         raise ValueError, "Unknown compression type %s" % compression
     if compression == "xz":
@@ -68,17 +70,18 @@ def compress(command, rootdir, outfile, compression="xz", compressargs=["-9"]):
         [p.kill() for p in (find, archive, comp) if p]
         return 1
 
-def mkcpio(rootdir, outfile, compression="xz", compressargs=["-9"]):
+def mkcpio(rootdir, outfile, compression="xz", compressargs=None):
     return compress(["cpio", "--null", "--quiet", "-H", "newc", "-o"],
                     rootdir, outfile, compression, compressargs)
 
-def mktar(rootdir, outfile, compression="xz", compressargs=["-9"]):
-    compressargs = compressargs or ["-9"]
+def mktar(rootdir, outfile, compression="xz", compressargs=None):
     return compress(["tar", "--no-recursion", "--selinux", "--acls", "--xattrs", "-cf-", "--null", "-T-"],
                     rootdir, outfile, compression, compressargs)
 
-def mksquashfs(rootdir, outfile, compression="default", compressargs=[]):
+def mksquashfs(rootdir, outfile, compression="default", compressargs=None):
     '''Make a squashfs image containing the given rootdir.'''
+    if compressargs is None:
+        compressargs = []
     if compression != "default":
         compressargs = ["-comp", compression] + compressargs
     return execWithRedirect("mksquashfs", [rootdir, outfile] + compressargs)
@@ -303,7 +306,9 @@ def round_to_blocks(size, blocksize):
     return size
 
 # TODO: move filesystem data outside this function
-def estimate_size(rootdir, graft={}, fstype=None, blocksize=4096, overhead=128):
+def estimate_size(rootdir, graft=None, fstype=None, blocksize=4096, overhead=128):
+    if graft is None:
+        graft = {}
     getsize = lambda f: os.lstat(f).st_size
     if fstype == "btrfs":
         overhead = 64*1024 # don't worry, it's all sparse
@@ -422,12 +427,16 @@ class PartitionMount(object):
 
 ######## Functions for making filesystem images ##########################
 
-def mkfsimage(fstype, rootdir, outfile, size=None, mkfsargs=[], mountargs="", graft={}):
+def mkfsimage(fstype, rootdir, outfile, size=None, mkfsargs=None, mountargs="", graft=None):
     '''Generic filesystem image creation function.
     fstype should be a filesystem type - "mkfs.${fstype}" must exist.
     graft should be a dict: {"some/path/in/image": "local/file/or/dir"};
       if the path ends with a '/' it's assumed to be a directory.
     Will raise CalledProcessError if something goes wrong.'''
+    if mkfsargs is None:
+        mkfsargs = []
+    if graft is None:
+        graft = {}
     preserve = (fstype not in ("msdos", "vfat"))
     if not size:
         size = estimate_size(rootdir, graft, fstype)
@@ -448,18 +457,18 @@ def mkfsimage(fstype, rootdir, outfile, size=None, mkfsargs=[], mountargs="", gr
     runcmd(["sync"])
 
 # convenience functions with useful defaults
-def mkdosimg(rootdir, outfile, size=None, label="", mountargs="shortname=winnt,umask=0077", graft={}):
+def mkdosimg(rootdir, outfile, size=None, label="", mountargs="shortname=winnt,umask=0077", graft=None):
     mkfsimage("msdos", rootdir, outfile, size, mountargs=mountargs,
               mkfsargs=["-n", label], graft=graft)
 
-def mkext4img(rootdir, outfile, size=None, label="", mountargs="", graft={}):
+def mkext4img(rootdir, outfile, size=None, label="", mountargs="", graft=None):
     mkfsimage("ext4", rootdir, outfile, size, mountargs=mountargs,
               mkfsargs=["-L", label, "-b", "1024", "-m", "0"], graft=graft)
 
-def mkbtrfsimg(rootdir, outfile, size=None, label="", mountargs="", graft={}):
+def mkbtrfsimg(rootdir, outfile, size=None, label="", mountargs="", graft=None):
     mkfsimage("btrfs", rootdir, outfile, size, mountargs=mountargs,
                mkfsargs=["-L", label], graft=graft)
 
-def mkhfsimg(rootdir, outfile, size=None, label="", mountargs="", graft={}):
+def mkhfsimg(rootdir, outfile, size=None, label="", mountargs="", graft=None):
     mkfsimage("hfsplus", rootdir, outfile, size, mountargs=mountargs,
               mkfsargs=["-v", label], graft=graft)
