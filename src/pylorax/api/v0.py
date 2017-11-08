641ea8c715273f364bcc6b78df1884c6a40c31df
Brian C. Lane <bcl@redhat.com>
2017-11-07 17:09:56 -0800

    Add /recipes/list route and tests
    Includes adding a lock for access to the git repo from the API.

--- src/pylorax/api/v0.py
+++ src/pylorax/api/v0.py
@@ -14,13 +14,14 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-from flask import jsonify
+from flask import jsonify, request
 
 # Use pykickstart to calculate disk image size
 from pykickstart.parser import KickstartParser
 from pykickstart.version import makeVersion, RHEL7
 
 from pylorax.api.crossdomain import crossdomain
+from pylorax.api.recipes import list_branch_files
 from pylorax.creator import DRACUT_DEFAULT, mount_boot_part_over_root
 from pylorax.creator import make_appliance, make_image, make_livecd, make_live_images
 from pylorax.creator import make_runtime, make_squashfs
@@ -29,6 +30,8 @@ from pylorax.imgutils import Mount, PartitionMount, umount
 from pylorax.installer import InstallError
 from pylorax.sysutils import joinpaths
 
+# The API functions don't actually get called by any code here
+# pylint: disable=unused-variable
 
 # no-virt mode doesn't need libvirt, so make it optional
 try:
@@ -42,3 +45,18 @@ def v0_api(api):
     @crossdomain(origin="*")
     def v0_status():
         return jsonify(build="devel", api="0", db_version="0", schema_version="0", db_supported=False)
+
+    @api.route("/api/v0/recipes/list")
+    @crossdomain(origin="*")
+    def v0_recipes_list():
+        """List the available recipes on a branch."""
+        try:
+            limit = int(request.args.get("limit", "20"))
+            offset = int(request.args.get("offset", "0"))
+        except ValueError:
+            # TODO return an error
+            pass
+
+        with api.config["GITLOCK"].lock:
+            recipes = map(lambda f: f[:-5], list_branch_files(api.config["GITLOCK"].repo, "master"))
+        return jsonify(recipes=recipes, limit=limit, offset=offset, total=len(recipes))
