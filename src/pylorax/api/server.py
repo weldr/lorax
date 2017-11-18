82da7d25102c7871c2736622a5eefb3453afc4ac
Brian C. Lane <bcl@redhat.com>
2017-11-17 16:34:18 -0800

    Add support for yum to lorax-composer
    This includes a new configuration file at /etc/lorax/composer.conf with
    built-in defaults. It also adds a YUMLOCK server config object so that
    request handlers can access the yum base object without interfering
    with each other.

--- src/pylorax/api/server.py
+++ src/pylorax/api/server.py
@@ -27,6 +27,7 @@ from pylorax.api.v0 import v0_api
 from pylorax.sysutils import joinpaths
 
 GitLock = namedtuple("GitLock", ["repo", "lock", "dir"])
+YumLock = namedtuple("YumLock", ["yb", "lock"])
 
 server = Flask(__name__)
 
