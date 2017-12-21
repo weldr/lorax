89251d090672b412913462d2b6316d377512efed
Alexander Todorov <atodorov@redhat.com>
2017-12-21 12:19:34 +0200

    Silence pocketlint bad-preconf-access warnings

--- src/pylorax/api/yumbase.py
+++ src/pylorax/api/yumbase.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+# pylint: disable=bad-preconf-access
+
 import logging
 log = logging.getLogger("lorax-composer")
 
