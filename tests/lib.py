03e6015f20e4a98b989f53944a51f572802cf814
Brian C. Lane <bcl@redhat.com>
2018-07-26 18:08:49 -0700

    Add a test library
    This adds empty __init__.py to tests so that a lib.py library of helper
    functions can be imported from the tests.
    
    Add captured_output to use with composer-cli tests to capture stdout/err
    output from the functions.
    
    (cherry picked from commit eeae331ba0af2609214fafbfad197c2b931e3675)

---
+++ tests/lib.py
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2018  Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+import sys
+from contextlib import contextmanager
+from StringIO import StringIO
+
+@contextmanager
+def captured_output():
+    new_out, new_err = StringIO(), StringIO()
+    old_out, old_err = sys.stdout, sys.stderr
+    try:
+        sys.stdout, sys.stderr = new_out, new_err
+        yield sys.stdout, sys.stderr
+    finally:
+        sys.stdout, sys.stderr = old_out, old_err
