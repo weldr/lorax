8786c9764c36052261f6ebf7a2b2c61fb8eae6d5
Brian C. Lane <bcl@redhat.com>
2018-08-06 12:31:52 -0700

    Add a test for invalid characters in the API route
    Currently the code is not UTF8 safe, so we need to return a clear error
    when invalid characters are passed in.
    
    This also adds tests for the routes to confirm that an error is
    correctly returned.
    
    (cherry picked from commit 74f5def3d4cad01ef879579edc8784be93faec2a)

---
+++ src/pylorax/api/regexes.py
@@ -0,0 +1,21 @@
+#
+# Copyright (C) 2018  Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+import re
+
+# These are the characters that we allow to be passed in via the
+# API calls.
+VALID_API_STRING = re.compile(r'^[a-zA-Z0-9_,.:*-]+$')
