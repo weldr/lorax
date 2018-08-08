ecdb9c85b3ec3759cbbe747565f1a656075d4777
Chris Lumens <clumens@redhat.com>
2018-08-08 10:22:22 -0400

    Change the error return type for bad limit= and offset=.
    Each element in the errors value is now a dict, with a msg field and an
    id field.  The id field contains a value out of errors.py that can be
    used by the front end to key on.  The msg field is the same as what's
    been there.
    
    The idea is to keep the number of IDs somewhat limited so there's not a
    huge number of things for the front end to know.

---
+++ src/pylorax/api/errors.py
@@ -0,0 +1,21 @@
+#
+# lorax-composer API server
+#
+# Copyright (C) 2018  Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Returned from the API when ?limit= or ?offset= is given something that does
+# not convert into an integer.
+BAD_LIMIT_OR_OFFSET = "BadLimitOrOffset"
