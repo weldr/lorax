cef073d83262bb7b1a2c23310a055dbcfc88582a
Brian C. Lane <bcl@redhat.com>
2018-08-13 16:49:52 -0700

    New lorax documentation - 28.17

--- docs/html/_static/documentation_options.js
+++ docs/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '28.16',
+    VERSION: '28.17',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
