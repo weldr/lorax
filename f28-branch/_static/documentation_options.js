b2fe7917cfc2a9bbe4f8a608b919602976c812a6
Brian C. Lane <bcl@redhat.com>
2018-09-06 10:42:44 -0700

    Add lorax 28.18 documentation

--- f28-branch/_static/documentation_options.js
+++ f28-branch/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '28.17',
+    VERSION: '28.18',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
