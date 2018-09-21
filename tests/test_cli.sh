ca1bf01b03adfc2cea6fc775c987f98b9c30cf7d
Alexander Todorov <atodorov@redhat.com>
2018-09-21 12:46:54 +0300

    New cli test covering basic compose commands
    - need to specify --sharedir so lorax-composer can find its
      kickstart files
    
    - each test script writes results into a separate directory to
      avoid a passing test overwriting the results from a failing one.
      To avoid reporting failures in case of previously failing tests
      (e.g. during development) remove the temporary directories holding
      tets results before execution!

--- tests/test_cli.sh
+++ tests/test_cli.sh
@@ -2,13 +2,12 @@
 # Note: execute this file from the project root directory
 
 # setup
-
-export TESTRESULT_BEAKERLIB_DIR=`mktemp -d /var/tmp/beakerlib-composer-XXXXXX`
+rm -rf /var/tmp/beakerlib-*/
 export top_srcdir=`pwd`
 . ./tests/testenv.sh
 
 # start the lorax-composer daemon
-./src/sbin/lorax-composer ./tests/pylorax/blueprints/ &
+./src/sbin/lorax-composer --sharedir ./share/ ./tests/pylorax/blueprints/ &
 
 # wait for the backend to become ready
 until curl --unix-socket /run/weldr/api.socket http://localhost:4000/api/status | grep '"db_supported":true'; do
@@ -18,10 +17,10 @@ done;
 
 # invoke cli/ tests
 ./tests/cli/test_blueprints_sanity.sh
-
+./tests/cli/test_compose_sanity.sh
 
 # look for failures
-grep RESULT_STRING $TESTRESULT_BEAKERLIB_DIR/TestResults | grep -v PASS && exit 1
+grep RESULT_STRING /var/tmp/beakerlib-*/TestResults | grep -v PASS && exit 1
 
 # explicit return code for Makefile
 exit 0
