41cbee247b6576889ae8aacf60315a85cc3f4184
Alexander Todorov <atodorov@redhat.com>
2018-09-21 12:46:54 +0300

    New cli test covering basic compose commands
    - need to specify --sharedir so lorax-composer can find its
      kickstart files
    
    - each test script writes results into a separate directory to
      avoid a passing test overwriting the results from a failing one.
      To avoid reporting failures in case of previously failing tests
      (e.g. during development) remove the temporary directories holding
      tets results before execution!

---
+++ tests/cli/test_compose_sanity.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Note: execute this file from the project root directory
+
+. /usr/share/beakerlib/beakerlib.sh
+
+CLI="./src/bin/composer-cli"
+
+
+rlJournalStart
+    rlPhaseStartTest "compose types"
+        rlAssertEquals "lists all supported types" \
+                "`$CLI compose types`" "Compose Types: ext4-filesystem, live-iso, partitioned-disk, qcow2, tar"
+    rlPhaseEnd
+
+    rlPhaseStartTest "compose start"
+        UUID=`$CLI --test=2 compose start http-server tar`
+        rlAssertEquals "exit code should be zero" $? 0
+
+        UUID=`echo $UUID | cut -f 2 -d' '`
+    rlPhaseEnd
+
+    rlPhaseStartTest "compose info"
+        if [ -n "$UUID" ]; then
+            rlRun -t -c "$CLI compose details $UUID | egrep 'RUNNING|WAITING'"
+        else
+            rlFail "Compose UUID is empty!"
+        fi
+    rlPhaseEnd
+
+    rlPhaseStartTest "compose image"
+        if [ -n "$UUID" ]; then
+            until $CLI compose details $UUID | grep FINISHED; do
+                sleep 5
+                rlLogInfo "Waiting for compose to finish ..."
+            done;
+
+            rlRun -t -c "$CLI compose image $UUID"
+            rlAssertExists "$UUID-root.tar.xz"
+        else
+            rlFail "Compose UUID is empty!"
+        fi
+    rlPhaseEnd
+
+rlJournalEnd
+rlJournalPrintText
