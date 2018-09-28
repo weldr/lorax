1ca0393771b6477efca0496fdcfac57024c7de62
David Shea <dshea@redhat.com>
2018-09-28 14:27:11 -0400

    Remove --fstype from the generated part line
    Instead of specifying the fstype, just let anaconda use the default.
    
    (cherry picked from commit 847fff4e11cf8a442a29e66fd2d28b82efbacc7a)
    
    Related: rhbz#1656105

--- src/pylorax/api/compose.py
+++ src/pylorax/api/compose.py
@@ -376,7 +376,7 @@ def start_build(cfg, yumlock, gitlock, branch, recipe_name, compose_type, test_m
         f.write('clearpart --all\n')
 
         # Write the root partition and it's size in MB (rounded up)
-        f.write('part / --fstype="ext4" --size=%d\n' % ceil(installed_size / 1024**2))
+        f.write('part / --size=%d\n' % ceil(installed_size / 1024**2))
 
         f.write(ks_template)
 
