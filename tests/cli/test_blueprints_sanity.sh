959589652b1c8034f121b3cd3d92f4b7d55e8349
Alexander Todorov <atodorov@redhat.com>
2018-10-02 23:30:02 +0300

    Execute bash tests for composer-cli
    these are built on top of beakerlib and we use its internal
    protocol to figure out the result without relying on the full
    test runner that is tipically used inside of a RHEL environment!
    
    Includes a disabled test snippet for Issue #460

---
+++ tests/cli/test_blueprints_sanity.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Note: execute this file from the project root directory
+
+. /usr/share/beakerlib/beakerlib.sh
+
+CLI="./src/bin/composer-cli"
+
+
+rlJournalStart
+    rlPhaseStartTest "blueprints list"
+        for bp in example-http-server example-development example-atlas; do
+            rlAssertEquals "blueprint list finds $bp" \
+                "`$CLI blueprints list | grep $bp`" "$bp"
+        done
+    rlPhaseEnd
+
+    rlPhaseStartTest "blueprints save"
+        rlRun -t -c "$CLI blueprints save example-http-server"
+        rlAssertExists "example-http-server.toml"
+        rlAssertGrep "example-http-server" "example-http-server.toml"
+        rlAssertGrep "httpd" "example-http-server.toml"
+
+        # non-existing blueprint
+# enable test for https://github.com/weldr/lorax/issues/460
+#        rlRun -t -c "$CLI blueprints save non-existing-bp" 1
+#        rlAssertNotExists "non-existing-bp.toml"
+    rlPhaseEnd
+
+    rlPhaseStartTest "blueprints push"
+
+        cat > beakerlib.toml << __EOF__
+name = "beakerlib"
+description = "Start building tests with beakerlib."
+version = "0.0.1"
+
+[[modules]]
+name = "beakerlib"
+version = "*"
+__EOF__
+
+        rlRun -t -c "$CLI blueprints push beakerlib.toml"
+        rlAssertEquals "pushed bp is found via list" "`$CLI blueprints list | grep beakerlib`" "beakerlib"
+    rlPhaseEnd
+
+rlJournalEnd
+rlJournalPrintText
---
+++ tests/test_cli.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Note: execute this file from the project root directory
+
+# setup
+
+export TESTRESULT_BEAKERLIB_DIR=`mktemp -d /var/tmp/beakerlib-composer-XXXXXX`
+export top_srcdir=`pwd`
+. ./tests/testenv.sh
+
+# start the lorax-composer daemon
+./src/sbin/lorax-composer ./tests/pylorax/blueprints/ &
+
+# wait for the backend to become ready
+until curl --unix-socket /run/weldr/api.socket http://localhost:4000/api/status | grep '"db_supported": true'; do
+    sleep 2
+    echo "DEBUG: Waiting for backend API to become ready before testing ..."
+done;
+
+# invoke cli/ tests
+./tests/cli/test_blueprints_sanity.sh
+
+
+# look for failures
+grep RESULT_STRING $TESTRESULT_BEAKERLIB_DIR/TestResults | grep -v PASS && exit 1
+
+# explicit return code for Makefile
+exit 0
