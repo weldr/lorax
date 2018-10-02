7c38d74c00775654ba686fa1521e1a746ce0f630
Alexander Todorov <atodorov@redhat.com>
2018-10-02 09:38:33 +0300

    New tests: build ext4-filesystem and partitioned-disk composes
    for the moment we just make sure these two can be built without
    visible errors.
    
    (cherry picked from commit 2eb62014db74977ccb5df9fcdbb1a98b70a34181)

---
+++ tests/cli/test_compose_partitioned-disk.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Note: execute this file from the project root directory
+
+#####
+#
+# Make sure a partitioned-disk compose can be built without errors!
+# Note: according to existing test plan we're not going to validate
+# direct usage-scenarios for this image type!
+#
+#####
+
+. /usr/share/beakerlib/beakerlib.sh
+
+CLI="./src/bin/composer-cli"
+
+
+rlJournalStart
+    rlPhaseStartTest "compose start"
+        UUID=`$CLI compose start example-http-server partitioned-disk`
+        rlAssertEquals "exit code should be zero" $? 0
+
+        UUID=`echo $UUID | cut -f 2 -d' '`
+    rlPhaseEnd
+
+    rlPhaseStartTest "compose finished"
+        if [ -n "$UUID" ]; then
+            until $CLI compose info $UUID | grep FINISHED; do
+                sleep 10
+                rlLogInfo "Waiting for compose to finish ..."
+            done;
+        else
+            rlFail "Compose UUID is empty!"
+        fi
+    rlPhaseEnd
+
+rlJournalEnd
+rlJournalPrintText
