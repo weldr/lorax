aa263398809dea094cc158e96c96da90799b3e2b
Alexander Todorov <atodorov@redhat.com>
2018-12-10 14:45:27 +0200

    Backport cloud image tests to RHEL 8
    Related: rhbz#1653934
    
    note: for now use Fedora 28 for the Docker container until
    CentOS 8 is released or we figure out how to use subscriptions
    for official RHEL 8 images.

--- tests/test_cli.sh
+++ tests/test_cli.sh
@@ -6,18 +6,40 @@ rm -rf /var/tmp/beakerlib-*/
 export top_srcdir=`pwd`
 . ./tests/testenv.sh
 
+BLUEPRINTS_DIR=`mktemp -d '/tmp/composer-blueprints.XXXXX'`
+cp ./tests/pylorax/blueprints/*.toml $BLUEPRINTS_DIR
+
+SHARE_DIR=`mktemp -d '/tmp/composer-share.XXXXX'`
+cp -R ./share/* $SHARE_DIR
+chmod a+rx -R $SHARE_DIR
+
 # start the lorax-composer daemon
-./src/sbin/lorax-composer --sharedir ./share/ ./tests/pylorax/blueprints/ &
+./src/sbin/lorax-composer --sharedir $SHARE_DIR $BLUEPRINTS_DIR &
 
 # wait for the backend to become ready
-until curl --unix-socket /run/weldr/api.socket http://localhost:4000/api/status | grep '"db_supported": true'; do
+tries=0
+until curl -m 15 --unix-socket /run/weldr/api.socket http://localhost:4000/api/status | grep 'db_supported.*true'; do
+    tries=$((tries + 1))
+    if [ $tries -gt 20 ]; then
+        exit 1
+    fi
     sleep 2
     echo "DEBUG: Waiting for backend API to become ready before testing ..."
 done;
 
-# invoke cli/ tests
-./tests/cli/test_blueprints_sanity.sh
-./tests/cli/test_compose_sanity.sh
+
+if [ -z "$*" ]; then
+    # invoke cli/ tests which can be executed without special preparation
+    ./tests/cli/test_blueprints_sanity.sh
+    ./tests/cli/test_compose_sanity.sh
+else
+    # execute other cli tests which need more adjustments in the calling environment
+    # or can't be executed inside Travis CI
+    for TEST in "$*"; do
+        ./$TEST
+    done
+fi
+
 
 # Stop lorax-composer and remove /run/weldr/api.socket
 pkill -9 lorax-composer
