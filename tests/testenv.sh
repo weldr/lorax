d950ecc4acdcf86ede9ced0dd0d22bbe1e7b052b
Alexander Todorov <atodorov@redhat.com>
2018-12-12 13:38:09 +0200

    Backport cloud image tests from master

---
+++ tests/testenv.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ -z "$top_srcdir" ]; then
+    echo "*** top_srcdir must be set"
+    exit 1
+fi
+
+# If no top_builddir is set, use top_srcdir
+: "${top_builddir:=$top_srcdir}"
+
+if [ -z "$PYTHONPATH" ]; then
+    PYTHONPATH="${top_builddir}/src/:${top_srcdir}/tests/lib"
+else
+    PYTHONPATH="${PYTHONPATH}:${top_srcdir}/src/:${top_srcdir}:${top_srcdir}/tests/lib"
+fi
+
+export PYTHONPATH
+export top_srcdir
+export top_builddir
--- tests/usercustomize.py
+++
@@ -1,8 +0,0 @@
-# Enable Python coverage for subprocesses. See:
-# http://nedbatchelder.com/code/coverage/subprocess.html
-
-try:
-    import coverage
-    coverage.process_startup()
-except ImportError:
-    pass
