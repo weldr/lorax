835c912e12096a199cf0de58e99dd8add381610c
Brian C. Lane <bcl@redhat.com>
2019-04-04 16:20:51 -0700

    Only use repos with valid urls for test_server.py
    libdnf-0.22.5-5 changed something and now the repos with fake urls are
    failing when loaded by test_server.py (they still work fine with
    test_projects.py) so only use the 'good' repos with the test_server.py
    tests -- the others weren't needed for any of its tests anyway.

--- tests/pylorax/test_server.py
+++ tests/pylorax/test_server.py
@@ -118,9 +118,9 @@ class ServerTestCase(unittest.TestCase):
 
         make_dnf_dirs(server.config["COMPOSER_CFG"], os.getuid(), os.getgid())
 
-        # copy over the test dnf repositories
+        # copy over the test_server dnf repositories
         dnf_repo_dir = server.config["COMPOSER_CFG"].get("composer", "repo_dir")
-        for f in glob("./tests/pylorax/repos/*.repo"):
+        for f in glob("./tests/pylorax/repos/server-*.repo"):
             shutil.copy2(f, dnf_repo_dir)
 
         # Modify fedora vs. rawhide tests when running on rawhide
