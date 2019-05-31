4bd03cb8f60a0b597b9945a264505b855545106c
Lars Karlitski <lars@karlitski.net>
2019-05-31 18:35:01 +0200

    Don't send CORS headers
    These are meant for web applications that are accessed by browsers, not
    REST APIs.

--- src/pylorax/api/__init__.py
+++ src/pylorax/api/__init__.py
@@ -15,6 +15,3 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-from pylorax.api.crossdomain import crossdomain
-
-__all__ = ["crossdomain"]
