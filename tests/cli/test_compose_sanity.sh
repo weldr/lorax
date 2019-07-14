e379d2b025254ebe730d7abb3bf5be65da3713d4
Lars Karlitski <lars@karlitski.net>
2019-07-14 23:57:52 +0200

    test_compose_sanity: Remove test mode test
    This is not testing an actual use case. We're testing the same things on
    real images.
    
    It was also testing internal state in `/var/lib`, which is not
    considered public API.

--- tests/cli/test_compose_sanity.sh
+++ tests/cli/test_compose_sanity.sh
@@ -14,41 +14,5 @@ rlJournalStart
         rlAssertEquals "lists all supported types" \
                 "`$CLI compose types | xargs`" "alibaba ami ext4-filesystem google hyper-v live-iso liveimg-tar openstack partitioned-disk qcow2 tar vhd vmdk"
     rlPhaseEnd
-
-    rlPhaseStartTest "compose start"
-        UUID=`$CLI --test=2 compose start example-http-server tar`
-        rlAssertEquals "exit code should be zero" $? 0
-
-        UUID=`echo $UUID | cut -f 2 -d' '`
-    rlPhaseEnd
-
-    rlPhaseStartTest "compose info"
-        if [ -n "$UUID" ]; then
-            rlRun -t -c "$CLI compose info $UUID | egrep 'RUNNING|WAITING'"
-        else
-            rlFail "Compose UUID is empty!"
-        fi
-    rlPhaseEnd
-
-    rlPhaseStartTest "compose image"
-        if [ -n "$UUID" ]; then
-            until $CLI compose info $UUID | grep 'FINISHED\|FAILED'; do
-                sleep 5
-                rlLogInfo "Waiting for compose to finish ..."
-            done;
-            check_compose_status "$UUID"
-
-            rlRun -t -c "$CLI compose image $UUID"
-            rlAssertExists "$UUID-root.tar.xz"
-
-            # because this path is listed in the documentation
-            rlAssertExists    "/var/lib/lorax/composer/results/$UUID/"
-            rlAssertExists    "/var/lib/lorax/composer/results/$UUID/root.tar.xz"
-            rlAssertNotDiffer "/var/lib/lorax/composer/results/$UUID/root.tar.xz" "$UUID-root.tar.xz"
-        else
-            rlFail "Compose UUID is empty!"
-        fi
-    rlPhaseEnd
-
 rlJournalEnd
 rlJournalPrintText
