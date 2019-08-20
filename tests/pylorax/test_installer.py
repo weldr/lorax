68f8668ffbfc31ea8b7f6ccbbd82c9c7948800b7
Brian C. Lane <bcl@redhat.com>
2019-08-20 09:25:36 -0700

    tests: Add tests for Vagrant functions in installer.py

--- tests/pylorax/test_installer.py
+++ tests/pylorax/test_installer.py
@@ -14,11 +14,13 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+import json
+import tempfile
 import os
 import unittest
 
 from ..lib import get_file_magic
-from pylorax.installer import make_ks_disk
+from pylorax.installer import make_ks_disk, create_vagrant_metadata, update_vagrant_metadata
 
 class InstallerTest(unittest.TestCase):
     @unittest.skipUnless(os.geteuid() == 0 and not os.path.exists("/.in-container"), "requires root privileges, and no containers")
@@ -28,3 +30,32 @@ class InstallerTest(unittest.TestCase):
         self.assertTrue(os.path.exists(ks_disk))
         file_details = get_file_magic(ks_disk)
         self.assertTrue("ext2 filesystem" in file_details, file_details)
+
+    def create_vagrant_md_test(self):
+        "Test Vagrant metadata creation"""
+        with tempfile.NamedTemporaryFile(mode="w+t", prefix="lmc-test-vagrant-") as f:
+            create_vagrant_metadata(f.name, 3192.7*1024**2)
+            f.flush()
+            f.seek(0)
+            md = json.load(f)
+            self.assertTrue(md is not None)
+            self.assertTrue("provider" in md)
+            self.assertEqual(md["provider"], "libvirt")
+            self.assertTrue("virtual_size" in md)
+            self.assertEqual(md["virtual_size"], 3269325)
+
+    def update_vagrant_md_test(self):
+        """Test updating Vagrant metadata"""
+        with tempfile.NamedTemporaryFile(mode="w+t", prefix="lmc-test-vagrant-") as f:
+            metadata = {"provider": "weldr", "format": "raw", "virtual_size": 0 }
+            json.dump(metadata, f, indent=4)
+            f.flush()
+            f.seek(0)
+
+            update_vagrant_metadata(f.name, 2892.7*1024**2)
+            md = json.load(f)
+            self.assertTrue(md is not None)
+            self.assertTrue("provider" in md)
+            self.assertEqual(md["provider"], "libvirt")
+            self.assertTrue("virtual_size" in md)
+            self.assertEqual(md["virtual_size"], 2962125)
