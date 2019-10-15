b8bf258a3c1be102b24d16f29319f5f6df0d6cc6
Brian C. Lane <bcl@redhat.com>
2019-10-15 16:48:13 -0700

    tests: Split testing the image into a separate script
    Nested virt is not reliable enough, especially on other arches, to rely
    on for testing the created images. This moves the test code into
    test_boot_* scripts to be run from inside the booted images.
    
    It also adds copying the results of the build into
    /var/tmp/test-results/, and includes the generated ssh key so that
    whatever boots the image can also log in.
    
    The tests/test_image.sh script has been added to handle running the
    test_boot_* scripts without any of the extra lorax-composer specific
    setup.

--- tests/cli/test_compose_live-iso.sh
+++ tests/cli/test_compose_live-iso.sh
@@ -15,40 +15,66 @@ set -e
 CLI="${CLI:-./src/bin/composer-cli}"
 
 rlJournalStart
-    rlPhaseStartSetup
-        rlAssertExists $QEMU_BIN
-    rlPhaseEnd
-
     rlPhaseStartTest "compose start"
         rlAssertEquals "SELinux operates in enforcing mode" "$(getenforce)" "Enforcing"
 
+        TMP_DIR=$(mktemp -d /tmp/composer.XXXXX)
+        SSH_KEY_DIR=$(mktemp -d /tmp/composer-ssh-keys.XXXXXX)
+
+        rlRun -t -c "ssh-keygen -t rsa -N '' -f $SSH_KEY_DIR/id_rsa"
+        PUB_KEY=$(cat "$SSH_KEY_DIR/id_rsa.pub")
+
+        cat > "$TMP_DIR/with-ssh.toml" << __EOF__
+name = "with-ssh"
+description = "HTTP image with SSH"
+version = "0.0.1"
+
+[[packages]]
+name = "httpd"
+version = "*"
+
+[[packages]]
+name = "openssh-server"
+version = "*"
+
+[[packages]]
+name = "beakerlib"
+version = "*"
+
+[customizations.services]
+enabled = ["sshd"]
+
+[[customizations.user]]
+name = "root"
+key = "$PUB_KEY"
+
+[customizations.kernel]
+append = "custom_cmdline_arg"
+__EOF__
+
+        rlRun -t -c "$CLI blueprints push $TMP_DIR/with-ssh.toml"
+
         # NOTE: live-iso.ks explicitly disables sshd but test_cli.sh enables it
-        UUID=`$CLI compose start example-http-server live-iso`
+        UUID=$($CLI compose start with-ssh live-iso)
         rlAssertEquals "exit code should be zero" $? 0
 
-        UUID=`echo $UUID | cut -f 2 -d' '`
+        UUID=$(echo "$UUID" | cut -f 2 -d' ')
     rlPhaseEnd
 
     rlPhaseStartTest "compose finished"
-        wait_for_compose $UUID
+        wait_for_compose "$UUID"
 
-        rlRun -t -c "$CLI compose image $UUID"
-        IMAGE="$UUID-live.iso"
-    rlPhaseEnd
-
-    rlPhaseStartTest "Start VM instance"
-        boot_image "-boot d -cdrom $IMAGE" 120
-    rlPhaseEnd
-
-    rlPhaseStartTest "Verify VM instance"
-        # run generic tests to verify the instance
-        ROOT_ACCOUNT_LOCKED=0 verify_image liveuser localhost "-p $SSH_PORT"
+        # Save the results for boot test
+        rlAssertExists "/var/lib/lorax/composer/results/$UUID/live.iso"
+        rlRun -t -c "mkdir -p /var/tmp/test-results/"
+        rlRun -t -c "cp /var/lib/lorax/composer/results/$UUID/live.iso /var/tmp/test-results/"
+        # Include the ssh key needed to log into the image
+        rlRun -t -c "cp $SSH_KEY_DIR/* /var/tmp/test-results"
     rlPhaseEnd
 
     rlPhaseStartCleanup
-        rlRun -t -c "killall -9 qemu-system-$(uname -m)"
         rlRun -t -c "$CLI compose delete $UUID"
-        rlRun -t -c "rm -rf $IMAGE"
+        rlRun -t -c "rm -rf $TMP_DIR $SSH_KEY_DIR"
     rlPhaseEnd
 
 rlJournalEnd
