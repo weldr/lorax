d5de6bb7a417c284f8063416202c6460a7c4e4ff
Alexander Todorov <atodorov@redhat.com>
2019-11-06 14:24:30 +0200

    DRY when setting up, running & parsing results for beakerlib tests
    Cherry-picked from b197e448ff5d8f6913c36dc7e3f835f3a589e934
    
    Related: rhbz#1769525

--- tests/test_image.sh
+++ tests/test_image.sh
@@ -1,26 +1,14 @@
 #!/bin/bash
 # Note: execute this file from the project root directory
+# Used for running a beakerlib test script inside a running VM
+# without setting up composer first!
 
 set -eu
 
 . $(dirname $0)/cli/lib/lib.sh
 
-export BEAKERLIB_DIR=$(mktemp -d /tmp/composer-test.XXXXXX)
-export BEAKERLIB_JOURNAL=0
-if [ -z "$*" ]; then
-    echo "test_image.sh requires a test to execute"
-else
-    # execute tests
-    for TEST in "$@"; do
-        $TEST
-    done
-fi
+setup_beakerlib_env
 
-. $BEAKERLIB_DIR/TestResults
+run_beakerlib_tests "$@"
 
-if [ $TESTRESULT_RESULT_ECODE != 0 ]; then
-  echo "Test failed. Leaving log in $BEAKERLIB_DIR"
-  exit $TESTRESULT_RESULT_ECODE
-fi
-
-rm -rf $BEAKERLIB_DIR
+parse_beakerlib_results
