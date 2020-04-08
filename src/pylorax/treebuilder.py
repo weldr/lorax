f7a60f88fb19ef0d6ffd5515969f8e03b325d1ae
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2020-04-08 10:07:39 +0200

    treebuilder: make PEP8 happier

--- src/pylorax/treebuilder.py
+++ src/pylorax/treebuilder.py
@@ -18,6 +18,7 @@
 # Author(s):  Will Woods <wwoods@redhat.com>
 
 import logging
+
 logger = logging.getLogger("pylorax.treebuilder")
 
 import os, re
@@ -34,40 +35,46 @@ import pylorax.imgutils as imgutils
 from pylorax.executils import runcmd, runcmd_output, execWithCapture
 
 templatemap = {
-    'i386':    'x86.tmpl',
-    'x86_64':  'x86.tmpl',
+    'i386': 'x86.tmpl',
+    'x86_64': 'x86.tmpl',
     'ppc64le': 'ppc64le.tmpl',
-    's390':    's390.tmpl',
-    's390x':   's390.tmpl',
+    's390': 's390.tmpl',
+    's390x': 's390.tmpl',
     'aarch64': 'aarch64.tmpl',
-    'arm':     'arm.tmpl',
-    'armhfp':  'arm.tmpl',
+    'arm': 'arm.tmpl',
+    'armhfp': 'arm.tmpl',
 }
+
 
 def generate_module_info(moddir, outfile=None):
     def module_desc(mod):
         output = runcmd_output(["modinfo", "-F", "description", mod])
         return output.strip()
+
     def read_module_set(name):
-        return set(l.strip() for l in open(joinpaths(moddir,name)) if ".ko" in l)
-    modsets = {'scsi':read_module_set("modules.block"),
-               'eth':read_module_set("modules.networking")}
+        return set(
+            l.strip() for l in open(joinpaths(moddir, name)) if ".ko" in l)
+
+    modsets = {'scsi': read_module_set("modules.block"),
+               'eth': read_module_set("modules.networking")}
 
     modinfo = list()
     for root, _dirs, files in os.walk(moddir):
         for modtype, modset in modsets.items():
             for mod in modset.intersection(files):  # modules in this dir
-                (name, _ext) = os.path.splitext(mod) # foo.ko -> (foo, .ko)
-                desc = module_desc(joinpaths(root,mod)) or "%s driver" % name
+                (name, _ext) = os.path.splitext(mod)  # foo.ko -> (foo, .ko)
+                desc = module_desc(joinpaths(root, mod)) or "%s driver" % name
                 modinfo.append(dict(name=name, type=modtype, desc=desc))
 
-    out = open(outfile or joinpaths(moddir,"module-info"), "w")
+    out = open(outfile or joinpaths(moddir, "module-info"), "w")
     out.write("Version 0\n")
     for mod in sorted(modinfo, key=lambda m: m.get('name')):
         out.write('{name}\n\t{type}\n\t"{desc:.65}"\n'.format(**mod))
 
+
 class RuntimeBuilder(object):
-    '''Builds the anaconda runtime image.'''
+    """Builds the anaconda runtime image."""
+
     def __init__(self, product, arch, dbo, templatedir=None,
                  installpkgs=None, excludepkgs=None,
                  add_templates=None,
@@ -100,14 +107,16 @@ class RuntimeBuilder(object):
         q = self.dbo.sack.query()
         a = q.available()
         pkgs = sorted([p.name for p in a.filter(provides='system-release')
-                                    if not p.name.startswith("generic")])
+                       if not p.name.startswith("generic")])
         if not pkgs:
-            logger.error("No system-release packages found, could not get the release")
+            logger.error(
+                "No system-release packages found, could not get the release")
             return
 
         logger.debug("system-release packages: %s", pkgs)
         if self.vars.product.variant:
-            variant = [p for p in pkgs if p.endswith("-"+self.vars.product.variant.lower())]
+            variant = [p for p in pkgs if
+                       p.endswith("-" + self.vars.product.variant.lower())]
             if variant:
                 release = variant[0]
         if not release:
@@ -122,7 +131,7 @@ class RuntimeBuilder(object):
         self._runner.installpkg('%s-logos' % release)
 
     def install(self):
-        '''Install packages and do initial setup with runtime-install.tmpl'''
+        """Install packages and do initial setup with runtime-install.tmpl"""
         self._install_branding()
         if len(self._installpkgs) > 0:
             self._runner.installpkg(*self._installpkgs)
@@ -133,7 +142,7 @@ class RuntimeBuilder(object):
             self._runner.run(tmpl, **self.add_template_vars)
 
     def writepkglists(self, pkglistdir):
-        '''debugging data: write out lists of package contents'''
+        """debugging data: write out lists of package contents"""
         if not os.path.isdir(pkglistdir):
             os.makedirs(pkglistdir)
         q = self.dbo.sack.query()
@@ -143,9 +152,9 @@ class RuntimeBuilder(object):
                     fobj.write("{0}\n".format(fname))
 
     def postinstall(self):
-        '''Do some post-install setup work with runtime-postinstall.tmpl'''
+        """Do some post-install setup work with runtime-postinstall.tmpl"""
         # copy configdir into runtime root beforehand
-        configdir = joinpaths(self._runner.templatedir,"config_files")
+        configdir = joinpaths(self._runner.templatedir, "config_files")
         configdir_path = "tmp/config_files"
         fullpath = joinpaths(self.vars.root, configdir_path)
         if os.path.exists(fullpath):
@@ -154,11 +163,11 @@ class RuntimeBuilder(object):
         self._runner.run("runtime-postinstall.tmpl", configdir=configdir_path)
 
     def cleanup(self):
-        '''Remove unneeded packages and files with runtime-cleanup.tmpl'''
+        """Remove unneeded packages and files with runtime-cleanup.tmpl"""
         self._runner.run("runtime-cleanup.tmpl")
 
     def verify(self):
-        '''Ensure that contents of the installroot can run'''
+        """Ensure that contents of the installroot can run"""
         status = True
 
         ELF_MAGIC = b'\x7fELF'
@@ -169,7 +178,8 @@ class RuntimeBuilder(object):
         elf_files = []
         usr_bin = Path(self.vars.root + '/usr/bin')
         usr_sbin = Path(self.vars.root + '/usr/sbin')
-        for path in (str(x) for x in itertools.chain(usr_bin.iterdir(), usr_sbin.iterdir()) \
+        for path in (str(x) for x in
+                     itertools.chain(usr_bin.iterdir(), usr_sbin.iterdir()) \
                      if x.is_file()):
             with open(path, "rb") as f:
                 magic = f.read(4)
@@ -186,7 +196,8 @@ class RuntimeBuilder(object):
 
                     # Does the path exist?
                     if not os.path.exists(self.vars.root + shabang):
-                        logger.error('%s, needed by %s, does not exist', shabang, path)
+                        logger.error('%s, needed by %s, does not exist',
+                                     shabang, path)
                         status = False
 
         # Now, run ldd on all the ELF files
@@ -194,24 +205,27 @@ class RuntimeBuilder(object):
         # At least one thing in the list isn't going to be a dynamic executable,
         # so use execWithCapture to ignore the exit code.
         filename = ''
-        for line in execWithCapture('ldd', elf_files, root=self.vars.root,
-                log_output=False, filter_stderr=True).split('\n'):
+        for line in execWithCapture(
+                'ldd', elf_files, root=self.vars.root, log_output=False,
+                filter_stderr=True).split('\n'):
             if line and not line[0].isspace():
                 # New filename header, strip the : at the end and save
                 filename = line[:-1]
             elif 'not found' in line:
-                logger.error('%s, needed by %s, not found', line.split()[0], filename)
+                logger.error('%s, needed by %s, not found', line.split()[0],
+                             filename)
                 status = False
 
         return status
 
     def writepkgsizes(self, pkgsizefile):
-        '''debugging data: write a big list of pkg sizes'''
+        """debugging data: write a big list of pkg sizes"""
         fobj = open(pkgsizefile, "w")
         getsize = lambda f: os.lstat(f).st_size if os.path.exists(f) else 0
         q = self.dbo.sack.query()
         for p in sorted(q.installed()):
-            pkgsize = sum(getsize(joinpaths(self.vars.root,f)) for f in p.files)
+            pkgsize = sum(
+                getsize(joinpaths(self.vars.root, f)) for f in p.files)
             fobj.write("{0.name}.{0.arch}: {1}\n".format(p, pkgsize))
 
     def generate_module_data(self):
@@ -221,9 +235,11 @@ class RuntimeBuilder(object):
             ksyms = joinpaths(root, "boot/System.map-%s" % kernel.version)
             logger.info("doing depmod and module-info for %s", kernel.version)
             runcmd(["depmod", "-a", "-F", ksyms, "-b", root, kernel.version])
-            generate_module_info(moddir+kernel.version, outfile=moddir+"module-info")
+            generate_module_info(moddir + kernel.version,
+                                 outfile=moddir + "module-info")
 
-    def create_squashfs_runtime(self, outfile="/var/tmp/squashfs.img", compression="xz", compressargs=None, size=2):
+    def create_squashfs_runtime(self, outfile="/var/tmp/squashfs.img",
+                                compression="xz", compressargs=None, size=2):
         """Create a plain squashfs runtime"""
         compressargs = compressargs or []
         os.makedirs(os.path.dirname(outfile))
@@ -231,7 +247,8 @@ class RuntimeBuilder(object):
         # squash the rootfs
         imgutils.mksquashfs(self.vars.root, outfile, compression, compressargs)
 
-    def create_ext4_runtime(self, outfile="/var/tmp/squashfs.img", compression="xz", compressargs=None, size=2):
+    def create_ext4_runtime(self, outfile="/var/tmp/squashfs.img",
+                            compression="xz", compressargs=None, size=2):
         """Create a squashfs compressed ext4 runtime"""
         # make live rootfs image - must be named "LiveOS/rootfs.img" for dracut
         compressargs = compressargs or []
@@ -240,7 +257,8 @@ class RuntimeBuilder(object):
 
         # Catch problems with the rootfs being too small and clearly log them
         try:
-            imgutils.mkrootfsimg(self.vars.root, joinpaths(workdir, "LiveOS/rootfs.img"),
+            imgutils.mkrootfsimg(self.vars.root,
+                                 joinpaths(workdir, "LiveOS/rootfs.img"),
                                  "Anaconda", size=size)
         except CalledProcessError as e:
             if e.stdout and "No space left on device" in e.stdout:
@@ -258,11 +276,15 @@ class RuntimeBuilder(object):
         """
         self.dbo.close()
 
+
 class TreeBuilder(object):
-    '''Builds the arch-specific boot images.
-    inroot should be the installtree root (the newly-built runtime dir)'''
-    def __init__(self, product, arch, inroot, outroot, runtime, isolabel, domacboot=True, doupgrade=True,
-                 templatedir=None, add_templates=None, add_template_vars=None, workdir=None, extra_boot_args=""):
+    """Builds the arch-specific boot images.
+    inroot should be the installtree root (the newly-built runtime dir)"""
+
+    def __init__(self, product, arch, inroot, outroot, runtime, isolabel,
+                 domacboot=True, doupgrade=True,
+                 templatedir=None, add_templates=None, add_template_vars=None,
+                 workdir=None, extra_boot_args=""):
 
         # NOTE: if you pass an arg named "runtime" to a mako template it'll
         # clobber some mako internal variables - hence "runtime_img".
@@ -270,10 +292,12 @@ class TreeBuilder(object):
                                runtime_base=basename(runtime),
                                inroot=inroot, outroot=outroot,
                                basearch=arch.basearch, libdir=arch.libdir,
-                               isolabel=isolabel, udev=udev_escape, domacboot=domacboot, doupgrade=doupgrade,
+                               isolabel=isolabel, udev=udev_escape,
+                               domacboot=domacboot, doupgrade=doupgrade,
                                workdir=workdir, lower=string_lower,
                                extra_boot_args=extra_boot_args)
-        self._runner = LoraxTemplateRunner(inroot, outroot, templatedir=templatedir)
+        self._runner = LoraxTemplateRunner(inroot, outroot,
+                                           templatedir=templatedir)
         self._runner.defaults = self.vars
         self.add_templates = add_templates or []
         self.add_template_vars = add_template_vars or {}
@@ -285,7 +309,7 @@ class TreeBuilder(object):
         return findkernels(root=self.vars.inroot)
 
     def rebuild_initrds(self, add_args=None, backup="", prefix=""):
-        '''Rebuild all the initrds in the tree. If backup is specified, each
+        """Rebuild all the initrds in the tree. If backup is specified, each
         initrd will be renamed with backup as a suffix before rebuilding.
         If backup is empty, the existing initrd files will be overwritten.
         If suffix is specified, the existing initrd is untouched and a new
@@ -293,7 +317,7 @@ class TreeBuilder(object):
 
         If the initrd doesn't exist its name will be created based on the
         name of the kernel.
-        '''
+        """
         add_args = add_args or []
         dracut = ["dracut", "--nomdadmconf", "--nolvmconf"] + add_args
         if not backup:
@@ -303,11 +327,12 @@ class TreeBuilder(object):
             raise Exception("No kernels found, cannot rebuild_initrds")
 
         # Hush some dracut warnings. TODO: bind-mount proc in place?
-        open(joinpaths(self.vars.inroot,"/proc/modules"),"w")
+        open(joinpaths(self.vars.inroot, "/proc/modules"), "w")
         for kernel in self.kernels:
             if prefix:
                 idir = os.path.dirname(kernel.path)
-                outfile = joinpaths(idir, prefix+'-'+kernel.version+'.img')
+                outfile = joinpaths(idir,
+                                    prefix + '-' + kernel.version + '.img')
             elif hasattr(kernel, "initrd"):
                 # If there is an existing initrd, use that
                 outfile = kernel.initrd.path
@@ -322,7 +347,7 @@ class TreeBuilder(object):
             cmd = dracut + [outfile, kernel.version]
             runcmd(cmd, root=self.vars.inroot)
 
-        os.unlink(joinpaths(self.vars.inroot,"/proc/modules"))
+        os.unlink(joinpaths(self.vars.inroot, "/proc/modules"))
 
     def build(self):
         templatefile = templatemap[self.vars.arch.basearch]
@@ -371,6 +396,7 @@ class TreeBuilder(object):
                                 dracut_path]
         return dracut_commands
 
+
 #### TreeBuilder helper functions
 
 def findkernels(root="/", kdir="boot"):
@@ -384,14 +410,14 @@ def findkernels(root="/", kdir="boot"):
         match = kre.match(f)
         if match:
             kernel = DataHolder(path=joinpaths(kdir, f))
-            kernel.update(match.groupdict()) # sets version, arch, flavor
+            kernel.update(match.groupdict())  # sets version, arch, flavor
             kernels.append(kernel)
 
     # look for associated initrd/initramfs/etc.
     for kernel in kernels:
         for f in bootfiles:
-            if f.endswith('-'+kernel.version+'.img'):
-                imgtype, _rest = f.split('-',1)
+            if f.endswith('-' + kernel.version + '.img'):
+                imgtype, _rest = f.split('-', 1)
                 # special backwards-compat case
                 if imgtype == 'initramfs':
                     imgtype = 'initrd'
@@ -400,20 +426,22 @@ def findkernels(root="/", kdir="boot"):
     logger.debug("kernels=%s", kernels)
     return kernels
 
+
 # udev whitelist: 'a-zA-Z0-9#+.:=@_-' (see is_whitelisted in libudev-util.c)
-udev_blacklist=' !"$%&\'()*,/;<>?[\\]^`{|}~' # ASCII printable, minus whitelist
-udev_blacklist += ''.join(chr(i) for i in range(32)) # ASCII non-printable
+udev_blacklist = ' !"$%&\'()*,/;<>?[\\]^`{|}~'  # ASCII printable, minus whitelist
+udev_blacklist += ''.join(chr(i) for i in range(32))  # ASCII non-printable
+
+
 def udev_escape(label):
     out = ''
     for ch in label:
         out += ch if ch not in udev_blacklist else '\\x%02x' % ord(ch)
     return out
 
+
 def string_lower(string):
     """ Return a lowercase string.
-
     :param string: String to lowercase
-
     This is used as a filter in the templates.
     """
     return string.lower()
