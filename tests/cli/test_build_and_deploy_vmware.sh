e04523f3ba1f11648a6a7f82cd7ec42ad3d0f1e8
Matej Marusak <mmarusak@redhat.com>
2020-08-04 12:51:10 +0200

    test: Don't upgrade pip packages (#1057)
    See https://github.com/cockpit-project/bots/pull/1115
    
    Related: rhbz#1785159

--- tests/cli/test_build_and_deploy_vmware.sh
+++ tests/cli/test_build_and_deploy_vmware.sh
@@ -60,7 +60,7 @@ rlJournalStart
         rlRun -t -c "virtualenv $VENV"
         source $VENV/bin/activate
 
-        rlRun -t -c "pip install --upgrade pip setuptools"
+        rlRun -t -c "pip install pip setuptools"
         rlRun -t -c "pip install pyvmomi"
 
         TMP_DIR=`mktemp -d /tmp/composer-vmware.XXXXX`
