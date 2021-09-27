16b76846c6e989a4f8351b8a780a046fe9793d98
Brian C. Lane <bcl@redhat.com>
2021-09-27 09:06:48 -0700

    tests: Ignore new pylint warnings
    Newer versions of pylint complain about u'' not being needed and using
    open without setting encoding. These are valid warnings, but do not
    effect the operation of lorax so they have been added to the
    FalsePositive list in runpylint.py
    
    (cherry picked from commit 49ff2c7d5c4467aeba35aff3e1d50e67c9d8daf5)

--- tests/pylint/runpylint.py
+++ tests/pylint/runpylint.py
@@ -3,6 +3,7 @@
 import sys
 
 from pocketlint import FalsePositive, PocketLintConfig, PocketLinter
+import pylint
 
 class LoraxLintConfig(PocketLintConfig):
     def __init__(self):
@@ -13,6 +14,8 @@ class LoraxLintConfig(PocketLintConfig):
                                 # See https://bugzilla.redhat.com/show_bug.cgi?id=1739167
                                 FalsePositive(r"Module 'rpm' has no '.*' member"),
                                 FalsePositive(r"raise-missing-from"),
+                                FalsePositive(r"redundant-u-string-prefix"),
+                                FalsePositive(r"unspecified-encoding"),
                               ]
 
     @property
@@ -45,6 +48,7 @@ class LoraxLintConfig(PocketLintConfig):
         return retval
 
 if __name__ == "__main__":
+    print("INFO: Using pylint v%s" % pylint.__version__)
     conf = LoraxLintConfig()
     linter = PocketLinter(conf)
     rc = linter.run()
