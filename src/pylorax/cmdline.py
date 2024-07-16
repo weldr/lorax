weldr/lorax

commit 01dd27a97c334c823903f7779d1b589cf288cc92

Replace squashfs_only with rootfs_type
The cmdline switch, --squashfs-only, now sets this and the new
--rootfs-type can be used to select other rootfs types. Currently only
squashfs and squashfs-ext4 are actually implemented.

This also changes the default to a plain squashfs without the wrapped
ext4 image. This speeds up image creation and there is no longer a
reason to use the wrapped ext4 method.

src/pylorax/cmdline.py
@@ -22,7 +22,7 @@ import os
 import sys
 import argparse
 
-from pylorax import DEFAULT_RELEASEVER, vernum
+from pylorax import DEFAULT_RELEASEVER, ROOTFSTYPES, vernum
 
 version = "{0}-{1}".format(os.path.basename(sys.argv[0]), vernum)
 
@@ -109,10 +109,14 @@ def lorax_parser(dracut_default=""):
                           help="Do not verify SSL certificates")
     optional.add_argument("--dnfplugin", action="append", default=[], dest="dnfplugins",
                           help="Enable a DNF plugin by name/glob, or * to enable all of them.")
-    optional.add_argument("--squashfs-only", action="store_true", default=False,
+    optional.add_argument("--squashfs-only", action="store_const", const="squashfs",
+                          default="squashfs", dest="rootfs_type",
                           help="Use a plain squashfs filesystem for the runtime.")
     optional.add_argument("--skip-branding", action="store_true", default=False,
                           help="Disable automatic branding package selection. Use --installpkgs to add custom branding.")
+    optional.add_argument("--rootfs-type", metavar="ROOTFSTYPE", default="squashfs",
+                          dest="rootfs_type",
+                          help="Type of rootfs: %s" % ",".join(ROOTFSTYPES))
 
     # dracut arguments
     dracut_group = parser.add_argument_group("dracut arguments: (default: %s)" % dracut_default)
@@ -324,8 +328,11 @@ def lmc_parser(dracut_default=""):
     parser.add_argument("--releasever", default=DEFAULT_RELEASEVER,
                         help="substituted for @VERSION@ in bootloader config files")
     parser.add_argument("--volid", default=None, help="volume id")
-    parser.add_argument("--squashfs-only", action="store_true", default=False,
+    parser.add_argument("--squashfs-only", action="store_const", const="squashfs",
+                        dest="rootfs_type",
                         help="Use a plain squashfs filesystem for the runtime.")
+    parser.add_argument("--rootfs-type", metavar="ROOTFSTYPE", default="squashfs",
+                        help="Type of rootfs: %s" % ",".join(ROOTFSTYPES))
     parser.add_argument("--timeout", default=None, type=int,
                         help="Cancel installer after X minutes")
 

src/pylorax/__init__.py
@@ -64,6 +64,8 @@ DRACUT_DEFAULT = ["--xz", "--install", "/.buildstamp", "--no-early-microcode", "
 DEFAULT_PLATFORM_ID = "platform:f41"
 DEFAULT_RELEASEVER = "41"
 
+ROOTFSTYPES = ["squashfs", "squashfs-ext4", "erofs"]
+
 # XXX - Temporarily lifted from dnf.rpm module
 def _invert(dct):
     return {v: k for k in dct for v in dct[k]}
@@ -219,7 +221,7 @@ class Lorax(BaseLoraxClass):
             add_arch_template_vars=None,
             verify=True,
             user_dracut_args=None,
-            squashfs_only=False,
+            rootfs_type="squashfs",
             skip_branding=False):
 
         assert self._configured
@@ -353,16 +355,24 @@ class Lorax(BaseLoraxClass):
                 compressargs += ["-Xbcj", self.arch.bcj]
             else:
                 logger.info("no BCJ filter for arch %s", self.arch.basearch)
-        if squashfs_only:
-            # Create an ext4 rootfs.img and compress it with squashfs
+        if rootfs_type == "squashfs":
+            # Create a squashfs compressed rootfs.img
             rc = rb.create_squashfs_runtime(joinpaths(installroot,runtime),
                     compression=compression, compressargs=compressargs,
                     size=size)
-        else:
+        elif rootfs_type == "squashfs-ext4":
             # Create an ext4 rootfs.img and compress it with squashfs
             rc = rb.create_ext4_runtime(joinpaths(installroot,runtime),
                     compression=compression, compressargs=compressargs,
                     size=size)
+        elif rootfs_type == "erofs":
+            raise RuntimeError("erofs not yet implemented")
+            # Create a erofs compressed rootfs.img
+##            rc = rb.create_erofs_runtime(joinpaths(installroot,runtime),
+##                    compression=compression, compressargs=compressargs,
+##                    size=size)
+        else:
+            raise RuntimeError(f"{rootfs_type} is not a supported type for the root filesystem")
         if rc != 0:
             logger.error("rootfs.img creation failed. See program.log")
             sys.exit(1)

src/pylorax/creator.py
@@ -221,14 +221,18 @@ def make_runtime(opts, mount_dir, work_dir, size=None):
     rb = RuntimeBuilder(product, arch, skip_branding=True, root=mount_dir)
     compression, compressargs = squashfs_args(opts)
 
-    if opts.squashfs_only:
+    if opts.rootfs_type == "squashfs":
         log.info("Creating a squashfs only runtime")
         return rb.create_squashfs_runtime(joinpaths(work_dir, RUNTIME), size=size,
                   compression=compression, compressargs=compressargs)
-    else:
+    elif opts.rootfs_type == "squashfs-ext4":
         log.info("Creating a squashfs+ext4 runtime")
         return rb.create_ext4_runtime(joinpaths(work_dir, RUNTIME), size=size,
                   compression=compression, compressargs=compressargs)
+    elif opts.rootfs_type == "erofs":
+        raise RuntimeError("erofs not supported yet")
+    else:
+        raise RuntimeError(f"{opts.rootfs_type} is not a supported type for the root filesystem")
 
 
 def rebuild_initrds_for_live(opts, sys_root_dir, results_dir):

src/sbin/lorax
@@ -37,7 +37,7 @@ import shutil
 #import librepo
 
 import pylorax
-from pylorax import DRACUT_DEFAULT, log_selinux_state
+from pylorax import DRACUT_DEFAULT, ROOTFSTYPES, log_selinux_state
 from pylorax.cmdline import lorax_parser
 from pylorax.dnfbase import get_dnf_base_object
 
@@ -103,6 +103,9 @@ def main():
         parser.error("argument --dracut-arg: not allowed with argument --dracut-conf")
     if opts.dracut_conf and not os.path.exists(opts.dracut_conf):
         parser.error("dracut config file %s doesn't exist." % opts.dracut_conf)
+
+    if opts.rootfs_type not in ROOTFSTYPES:
+        parser.error("--rootfs-type must be one of %s" % ",".join(ROOTFSTYPES))
 
     setup_logging(opts)
     log.debug(opts)
@@ -201,7 +204,7 @@ def main():
               add_arch_template_vars=parsed_add_arch_template_vars,
               remove_temp=True, verify=opts.verify,
               user_dracut_args=user_dracut_args,
-              squashfs_only=opts.squashfs_only,
+              rootfs_type=opts.rootfs_type,
               skip_branding=opts.skip_branding)
 
     # Release the lock on the tempdir

tests/pylorax/test_creator.py
@@ -153,7 +153,7 @@ class CreatorTest(unittest.TestCase):
                 mkFakeBoot(mount_dir)
                 opts = DataHolder(project="Fedora", releasever="devel", compression="xz", compress_args=[],
                                   release="", variant="", bugurl="", isfinal=False,
-                                  arch="x86_64", squashfs_only=True)
+                                  arch="x86_64", rootfs_type="squashfs")
                 make_runtime(opts, mount_dir, work_dir)
 
                 # Make sure it made an install.img
@@ -178,7 +178,7 @@ class CreatorTest(unittest.TestCase):
                 mkFakeBoot(mount_dir)
                 opts = DataHolder(project="Fedora", releasever="devel", compression="xz", compress_args=[],
                                   release="", variant="", bugurl="", isfinal=False,
-                                  arch="x86_64", squashfs_only=False)
+                                  arch="x86_64", rootfs_type="squashfs-ext4")
                 make_runtime(opts, mount_dir, work_dir)
 
                 # Make sure it made an install.img

commit aafedbb30baf521a49823473e839abc87556709d

Accept but ignore the old --squashfs-only argument
01dd27a broke lorax due to some unspecified behavior in argparse
when two different arguments write to the same target. It added
a --rootfs-type argument with a string value and a default of
"squashfs", and changed --squashfs-only to write the value
"squashfs" to the same target (rootfs_type) if passed.

Unfortunately, if you do this *with --squashfs-only first and
--rootfs-type second*, then if neither --squashfs-only nor
--rootfs-type is passed, the value of rootfs_type in the
resulting args dict is None, not "squashfs" as expected.

If you reverse the order of the args - so --rootfs-type is first
and --squashfs-only is second - then if neither arg is passed,
the value of rootfs_type comes out as "squashfs" as expected.
So we *could* just swap the args in both places. However, this
strikes me as fragile as it seems like this behaviour is not
defined and could change unexpectedly.

It seems safer, to me, to accept --squashfs-only but simply
ignore it (since the default value of --rootfs-type is squashfs
anyway, and if someone were to specify both, I think it's sane
for --rootfs-type to 'win'). Weirdly, argparse doesn't seem to
have a built-in way to specify a completely no-op argument, so
let's just keep it as `store_true` like it used to be, and
simply never use the value.

Alternatively we could use parse_known_args instead of
parse_args and then check that the only unknown arg was
squashfs-only, but we'd have to do that in multiple places, this
seems a bit simpler.

Signed-off-by: Adam Williamson <awilliam@redhat.com>

.github/workflows/example-livemedia-creator.yml
@@ -44,14 +44,12 @@ jobs:
       ## Create the ISO
       - name: Create the custom ISO
           # --no-virt: Needed since we're in a container, no host CPU
-          # --squashfs-only: Just to speed things up, not required
         run: |
           livemedia-creator \
             --ks "${{ inputs.kickstart_path }}" \
             --no-virt \
             --make-iso \
             --iso-only \
-            --squashfs-only \
             --iso-name Fedora-custom-example.iso \
             --project Fedora \
             --volid "Fedora-${{ inputs.fedora_release }}" \

src/pylorax/cmdline.py
@@ -109,9 +109,8 @@ def lorax_parser(dracut_default=""):
                           help="Do not verify SSL certificates")
     optional.add_argument("--dnfplugin", action="append", default=[], dest="dnfplugins",
                           help="Enable a DNF plugin by name/glob, or * to enable all of them.")
-    optional.add_argument("--squashfs-only", action="store_const", const="squashfs",
-                          default="squashfs", dest="rootfs_type",
-                          help="Use a plain squashfs filesystem for the runtime.")
+    optional.add_argument("--squashfs-only", action="store_true",
+                          help="Ignored, provided for backward compatibility.")
     optional.add_argument("--skip-branding", action="store_true", default=False,
                           help="Disable automatic branding package selection. Use --installpkgs to add custom branding.")
     optional.add_argument("--rootfs-type", metavar="ROOTFSTYPE", default="squashfs",
@@ -328,9 +327,8 @@ def lmc_parser(dracut_default=""):
     parser.add_argument("--releasever", default=DEFAULT_RELEASEVER,
                         help="substituted for @VERSION@ in bootloader config files")
     parser.add_argument("--volid", default=None, help="volume id")
-    parser.add_argument("--squashfs-only", action="store_const", const="squashfs",
-                        dest="rootfs_type",
-                        help="Use a plain squashfs filesystem for the runtime.")
+    parser.add_argument("--squashfs-only", action="store_true",
+                          help="Ignored, provided for backward compatibility.")
     parser.add_argument("--rootfs-type", metavar="ROOTFSTYPE", default="squashfs",
                         help="Type of rootfs: %s" % ",".join(ROOTFSTYPES))
     parser.add_argument("--timeout", default=None, type=int,